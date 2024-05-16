0a5e1b1fb98dd1a9321046424a40d048d279fe5f
Gabriel Alecrim <eu@gabrielalecrim.com.br>
2024-05-16 12:56:31 -0300

    Inicio do projeto

--- src/Interface.py
+++ src/Interface.py
@@ -1,9 +1,12 @@
 import customtkinter
 import serial
 import time
+import serial.threaded
+from threading import Thread
 #import serial.tools.list_ports
 
-Data = serial.Serial('COM3', baudrate = 9600, timeout = 1)
+Data = serial.Serial('COM3', baudrate = 115200, timeout = 1)
+
 
 customtkinter.set_appearance_mode('dark')
 customtkinter.set_default_color_theme('green')
@@ -13,25 +16,29 @@ root.geometry('500x400')
 
 def switch_event():
     status = switch_var.get()
-    
+    if (not Data.is_open):
+        Data.open()
     if status == 'on':
         
         Data.write(b'1')
         print('led on')
-
     if status == 'off':
 
         Data.write(b'0')
         print('led off')
+
 def button_event():
     Data.write(b'3')
     time.sleep(0.1)
     freq = entry.get()
-    Data.write(freq)
+    #Data.write(freq)
     print('Frequencia set')
+
 
 frame = customtkinter.CTkFrame(master=root)
 frame.pack(pady=20, padx=60)
+
+
 
 entry = customtkinter.CTkEntry(frame, placeholder_text="Frequência")
 entry.pack(pady=20, padx=120)
@@ -41,4 +48,27 @@ switch_var = customtkinter.StringVar(value="on")
 switch = customtkinter.CTkSwitch(frame, text="", command=switch_event, variable=switch_var, onvalue="on", offvalue="off", switch_width=140, switch_height=60)
 switch.pack(pady=60, padx=120)
 
+def Loop1S():  
+    if(Data.in_waiting != 0):
+        if (not Data.is_open):
+            Data.open()
+        vari = Data.read_all()
+        vari = vari.decode()
+        print(vari)
+
+    root.after(ms=1000,func=Loop1S)
+
+root.after(ms=1000,func=Loop1S)
+
+car = "tensao:24/corrente:323/vel:23"
+car2 = (car.split("/"))
+print(car2)
+print(car2[0].split(":"))
+
 root.mainloop()
+
+
+
+
+
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,55 +1,58 @@
 #include <Arduino.h>
 
-char serialData;
-int freq = 10;
-char freqNew = 0;
-bool freqSetup = 0;
+
+const int valSol = 16;
+const int valProp = 17;
+int Channel = 1;
+int freq = 350;
+int res = 10;
+int state;
+
+
+void pwm1(int valProp1,int Channel1,int freq1,int res1){
+  pinMode(valProp1, OUTPUT);
+  
+  ledcSetup(Channel1, freq1, res1);
+  ledcAttachPin(valProp1, Channel1);
+}
 
 void setup()
 {
-
-    Serial.begin(9600);
+     //inicia comunicação serial
+    Serial.begin(115200);
+    pwm1(valProp, Channel,freq,res);
     pinMode(2, OUTPUT);
-    Serial.setTimeout(1);
+    pinMode(valSol, OUTPUT);
+}
+
+void serialMonitor()
+{
+    if(Serial.available() > 0)
+    {
+        char stateCase = Serial.read();
+        switch(stateCase)
+        {
+            case '1':
+            state = 1;
+            Serial.println("Ligado");
+            digitalWrite(2,1);
+            break;
+            case '0':
+            state = 0;
+            Serial.println("Desligado");
+            digitalWrite(2,0);
+            break;
+        }
+    }
+
 }
 
 void loop() 
 {
-  
-    if (Serial.available() > 0)
-    {
+    serialMonitor();    
+}
 
-        serialData = Serial.read();
+void loadVar()
+{
 
-        if (serialData == '1')
-        {
-            /*freqNew = Serial.read();
-
-            if(freqNew == '3')
-            {
-                while(freqSetup == 0)
-                {
-                    if(Serial.read() != '3')
-                    {
-                        freq = Serial.parseInt();
-                        freqSetup = 1;
-                    }
-                }
-            freqSetup = 0;
-            }
-            else
-            {*/
-            while(Serial.read() != '0')
-            {
-                digitalWrite(2, HIGH);
-                delayMicroseconds(freq);
-                digitalWrite(2, LOW);
-                delayMicroseconds(freq);
-            }
-            }
-    }
-    else if (serialData == '0')
-    {
-      digitalWrite(2, LOW);
-    }
 }
